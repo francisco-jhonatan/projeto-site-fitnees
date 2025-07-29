338e6669bb95f15081bf05a6b9709874693f49bc
zironzin <franciscozidaneamaral33@gmail.com>
2025-07-29 19:56:12 -0300

    atualização de users

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000
 
+const usersRouter = require('./routes/users')
+
+app.use('GET', '/users',usersRouter)
+app.use('GET', '/users/:id', usersRouter)
+app.use('GET', '/new', usersRouter)
+
 // Rota básica
 app.get('/', (req, res) => {
   res.send('Hello World!');
