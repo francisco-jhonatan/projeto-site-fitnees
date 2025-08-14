9631252605f3a4fff1b6ab57ca938f4c226e0b10
iagoruanlopes <ruaniago25@gmail.com>
2025-08-13 21:59:08 -0300

    Modal.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const personalsRoutes = require('./routes/personals'); // Importa a rota
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 const cors = require('cors')// acess-control-allow-origin
@@ -16,7 +17,12 @@ app.get('/', (req, res) => {
   res.send('Welgcfc to the homepage!');
 });
 
+mongoose.connect("mongodb+srv://ruaniago25:lUP9Qzr4fXewsyKY@personal.xatmdq5.mongodb.net/?retryWrites=true&w=majority&appName=personal")
+.then(()=>{
+  console.log("Conexão estabelecida")
 // Server rodando
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+})
+
