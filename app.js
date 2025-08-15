1e39887f867715871c1bc815b5670f0a7c15d6ac
zironzin <franciscozidaneamaral33@gmail.com>
2025-08-14 22:04:51 -0300

    mongoose2.0

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
+var personals = [
+  {
+    'id': 0,
+    'nome': 'Zidane',
+    'genero': 'Masculino',
+    'formacao': 'Crossfit',
+    'experiencia': 'Crosfit',
+    'horario': 'manhã, tarde e noite',
+    'atendimento': 'Academia',
+    'image': 'https://fly.metroimg.com/upload/q_85,w_700/https://uploads.metroimg.com/wp-content/uploads/2024/09/09122937/Zinedine-Zidane-1.jpg',
+    'contato': {
+      'telefone': '999999999',
+      'email': 'zidane@mail.com',
+      'instagram': 'instagram.com'
+    }
+  }, {
+    'id': 1,
+    'nome': 'Jhonatan',
+    'genero': 'Masculino',
+    'formacao': ['Crossfit'],
+    'experiencia': ['Crosfit'],
+    'horario': 'manhã, tarde e noite',
+    'atendimento': 'Academia',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm0Ee53h-sFdUnIwZBoacJzz6CvnXeFHDSnw&s',
+    'contato': {
+      'telefone': '999999999',
+      'email': 'zidane@mail.com',
+      'instagram': 'instagram.com'
+    }
+  }, {
+    'id': 2,
+    'nome': 'Antônio',
+    'genero': 'Masculino',
+    'formacao': ['Crossfit'],
+    'experiencia': ['Crosfit'],
+    'horario': 'manhã, tarde e noite',
+    'atendimento': 'Academia',
+    'image': 'https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2021/06/11347_52C1A39356496734.jpg?w=419&h=283&crop=0',
+    'contato': {
+      'telefone': '999999999',
+      'email': 'antony@mail.com',
+      'instagram': 'instagram.com'
+    }
+  }, {
+    'id': 3,
+    'nome': 'Iago',
+    'genero': 'Masculino',
+    'formacao': ['Crossfit'],
+    'experiencia': ['Crosfit'],
+    'horario': 'manhã, tarde e noite',
+    'atendimento': 'Academia',
+    'image': 'https://platform.polygon.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/16296627/return_jafar_disneyscreencaps.com_1578.jpg?quality=90&strip=all&crop=0,5.5166374781086,100,88.966725043783',
+    'contato': {
+      'telefone': '999999999',
+      'email': 'iagoruan@mail.com',
+      'instagram': 'instagram.com'
+    }
+  }
+]
+
 const express = require('express');
 const personalsRoutes = require('./routes/personals');
 const mongoose = require('mongoose');
-const { Schema } = mongoose; 
+const { Schema } = mongoose;
 const cors = require('cors');
 
 const app = express();
@@ -17,53 +77,62 @@ app.get('/', (req, res) => {
 });
 
 
-mongoose.connect('mongodb+srv://franciscojhonatan0077:G6dJyPyVxfT23fQU@projetoacademia.476jyg3.mongodb.net/?retryWrites=true&w=majority&appName=ProjetoAcademia')
-  .then(() => {
-    console.log("Conexão estabelecida");
-  })
-  .catch(err => {
-    console.error("Erro na conexão MongoDB:", err);
-  });
+// mongoose.connect('mongodb+srv://franciscojhonatan0077:G6dJyPyVxfT23fQU@projetoacademia.476jyg3.mongodb.net/?retryWrites=true&w=majority&appName=ProjetoAcademia')
+//   .then(() => {
+//     console.log("Conexão estabelecida");
+//   })
+//   .catch(err => {
+//     console.error("Erro na conexão MongoDB:", err);
+//   });
 
 
-const blogSchema = new Schema({
-  title: String,
-  slug: String,
-  published: Boolean,
-  author: String,
-  content: String,
-  tags: [String],
-  createdAt: Date,
-  updatedAt: Date,
-  comments: [{
-    user: String,
-    content: String,
-    votes: Number
-  }]
-});
 
-const Blog = mongoose.model('Blog', blogSchema);
+
+
+// const Blog = mongoose.model('Blog', blogSchema);
 
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://franciscojhonatan0077:G6dJyPyVxfT23fQU@projetoacademia.476jyg3.mongodb.net/?retryWrites=true&w=majority&appName=ProjetoAcademia/personal')
+
+  const personalSchema = new mongoose.Schema({
+    nome: String,
+    genero: String,
+    formacao: String,
+    experiencia: String,
+    horario: String,
+    atendimento: String,
+    image: String,
+    contato: {
+      telefone: String,
+      email: String,
+      instagram: String
+    }
+  });
+  const personal = mongoose.model('personal', personalSchema);
+  const p = new personal({
+    nome: personals[0].nome,
+    genero: personals[0].genero,
+    formacao: personals[0].formacao,
+    experiencia: personals[0].experiencia,
+    horario: personals[0].experiencia,
+    atendimento: personals[0].atendimento,
+    image: personals[0].image,
+    contato: {
+      telefone: personals[0].contato.telefone,
+      email: personals[0].contato.email,
+      instagram: personals[0].contato.instagram,
+    }
+
+  })
+  await p.save();
 
 
 
-// import mongoose from 'mongoose';
-// const { Schema } = mongoose;
-
-// const blogSchema = new Schema({
-//   title: String, // String is shorthand for {type: String}
-//   author: String,
-//   body: String,
-//   comments: [{ body: String, date: Date }],
-//   date: { type: Date, default: Date.now },
-//   hidden: Boolean,
-//   meta: {
-//     votes: Number,
-//     favs: Number
-//   }
-// });
+}
