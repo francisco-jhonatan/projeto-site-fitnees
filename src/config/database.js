4fbc5f38c5f1667877527a4d7222ebc1d41002ba
zironzin <franciscozidaneamaral33@gmail.com>
2025-08-24 19:51:51 -0300

    Modal alterado

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Atlas conectado com sucesso!');
+    await mongoose.connect(
+      "mongodb+srv://Amaral86:Nascimento14%23@amaral.534eoif.mongodb.net/?retryWrites=true&w=majority&appName=Amaral",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("MongoDB Atlas conectado com sucesso!");
   } catch (error) {
-    console.error('Erro ao conectar com o MongoDB Atlas:', error.message);
+    console.error("Erro ao conectar com o MongoDB Atlas:", error.message);
     // Encerra o processo com falha
     process.exit(1);
   }
